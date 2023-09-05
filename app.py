17d6695d3d73ef47198eabada1f49090b4985e36
SheikhFoysaldiu <foysal15-3442@diu.edu.bd>
2023-09-05 20:41:38 +0600

    Fix

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
+
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
@@ -43,9 +44,10 @@ if st.button('Predict'):
 
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
+
     # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+  
     result = model.predict(vector_input)[0]
     # 4. Display
     if result == 1:
